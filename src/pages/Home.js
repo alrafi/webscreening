ca2c4e9973d7bfdb870cff81d0aefffdc6e253ea
alrafi <hafisalrafi@gmail.com>
2020-08-06 12:05:51 +0700

    refactor showcase content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,14 @@
 import React from 'react';
 import '../styles/home.scss';
 import Header from '../components/Header';
-import Button from '../components/Button';
+import ShowcaseContent from '../components/ShowcaseContent';
 
 const Home = () => {
   return (
     <div className="container">
       <section className="showcase">
         <Header />
-
-        <div className="content">
-          <h1>
-            Omnichannel Messaging Platform for sales, marketing, and support
-          </h1>
-          <p>
-            Elevate your customer engagement experience by allowing your
-            customers to reach you from WhatsApp, Twitter, FB Messenger,
-            Telegram, LINE, and email, and also easily manage it from a single
-            dashboard.
-          </p>
-          <div className="button-content">
-            <Button type="secondary" size="large">
-              See Pricing
-            </Button>
-            <Button type="primary" size="large">
-              Get Started Now!
-            </Button>
-          </div>
-          <img
-            src={require('../assets/img/PNG/Group - assets@2x.png')}
-            alt="TapTalk"
-            className="showcase-image"
-          />
-        </div>
+        <ShowcaseContent />
       </section>
       <section className="weather"></section>
     </div>
