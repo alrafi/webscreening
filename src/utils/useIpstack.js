8b7369b8e6f7464363f71c7a8118a749b57f086a
alrafi <hafisalrafi@gmail.com>
2020-08-07 11:54:24 +0700

    add useEffect function to call ipstack api

---
+++ src/utils/useIpstack.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+
+const useIpstack = (ip) => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    if (ip !== null) {
+      console.log(ip);
+      (async () => {
+        try {
+          const response = await Axios.get(
+            `http://api.ipstack.com/${ip}?access_key=1c3c27a25e4b67ff625a89e56bf41dc5`
+          );
+          setLocation(response.data);
+        } catch (err) {
+          setLocation('N/A');
+        }
+      })();
+    }
+  }, [ip]);
+
+  return location;
+};
+
+export default useIpstack;
